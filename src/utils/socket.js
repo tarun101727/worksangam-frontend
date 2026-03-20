746d65bb631fbac6592999a7c926c021f49b410e
tarun101727 <gaddamtarun157@gmail.com>
2026-03-20 13:53:13 +0530

    Update socket.js

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -1,7 +1,9 @@
+// src/utils/socket.js
 import { io } from "socket.io-client";
 import Cookies from "js-cookie";
+import { BASE_URL } from "../config"; // make sure config.js exports BASE_URL
 
-export const socket = io("https://api.workSangam.in", {
+export const socket = io(BASE_URL, {
   withCredentials: true,
   auth: { userId: Cookies.get("userId") },
 });

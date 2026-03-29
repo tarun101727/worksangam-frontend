928affbac0a88443183f4710006ce7be92e4f97c
tarun101727 <gaddamtarun157@gmail.com>
2026-03-29 14:17:39 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,18 @@
-// main.jsx
-import React from "react";
-import ReactDOM from "react-dom/client";
-import AppWrapper from "./AppWrapper"; // ✅ import the wrapper
-import { BrowserRouter } from "react-router-dom";
-import AuthProvider from "./AuthProvider";
-import "./index.css";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import { BrowserRouter } from 'react-router-dom';
+import AuthProvider from './AuthProvider';
+import './index.css';
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <AuthProvider>
-      <BrowserRouter>
-        <AppWrapper />
-      </BrowserRouter>
+      
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      
     </AuthProvider>
   </React.StrictMode>
 );

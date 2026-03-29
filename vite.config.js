5b8b19dd78bcd02f35a69a72d37355f144c2dde1
tarun101727 <gaddamtarun157@gmail.com>
2026-03-29 16:34:41 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,25 +1,10 @@
-// vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig(({ command }) => {
-  // command === 'serve' → dev mode, command === 'build' → production
-  if (command === 'serve') {
-    return {
-      plugins: [react()],
-      server: {
-        host: true,   // only for local dev
-        port: 5173,   // only for local dev
-      },
-    };
-  } else {
-    // Production build config
-    return {
-      plugins: [react()],
-      build: {
-        outDir: 'dist',       // default, you can keep it
-        sourcemap: false,     // optional
-      },
-    };
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    host: true,
+    port: 5173,
   }
 });

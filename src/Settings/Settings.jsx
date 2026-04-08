22e87d45ff2e0d304d47e1a5b700df3d1c1bb9f1
tarun101727 <gaddamtarun157@gmail.com>
2026-04-08 20:06:39 +0530

    Update Settings.jsx

--- src/Settings/Settings.jsx
+++ src/Settings/Settings.jsx
@@ -2,9 +2,10 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 import { BASE_URL } from "../config";
 import { useNavigate } from "react-router-dom";
+import { useTranslation } from "react-i18next";
 
 export default function Settings() {
-
+    const { t } = useTranslation();
 const [user,setUser] = useState(null);
 const navigate = useNavigate();
 
@@ -23,34 +24,34 @@ if(!user) return null;
 return(
 <div className="min-h-screen text-white px-4 py-6">
 
-<h1 className="text-2xl font-bold mb-6">Settings</h1>
+<h1 className="text-2xl font-bold mb-6">{t("Settings")}</h1>
 
 <div className="space-y-4">
 <button
 onClick={()=>navigate("/buy-credits")}
 className="w-full text-left p-4 bg-green-900/30 rounded-xl border border-green-500/20"
 >
-Buy Credits 💳
+{t("Buy Credits")} 💳
 </button>
 <button
 onClick={()=>navigate("/settings/account")}
 className="w-full text-left p-4 bg-[#111827] rounded-xl border border-white/10"
 >
-Account Information
+{t("Account Information")}
 </button>
 
 <button
 onClick={()=>navigate("/settings/Email")}
 className="w-full text-left p-4 bg-[#111827] rounded-xl border border-white/10"
 >
-Change Email Address
+{t("Change Email Address")}
 </button>
 
 <button
 onClick={()=>navigate("/settings/Password")}
 className="w-full text-left p-4 bg-[#111827] rounded-xl border border-white/10"
 >
-Change Password
+{t("Change Password")}
 </button>
 
 
@@ -58,14 +59,14 @@ Change Password
 onClick={()=>navigate("/settings/logout")}
 className="w-full text-left p-4 bg-yellow-900/30 rounded-xl border border-yellow-500/20"
 >
-Logout
+{t("Logout")}
 </button>
 
 <button
 onClick={()=>navigate("/settings/delete")}
 className="w-full text-left p-4 bg-red-900/30 rounded-xl border border-red-500/20"
 >
-Delete Account
+{t("Delete Account")}
 </button>
 
 </div>

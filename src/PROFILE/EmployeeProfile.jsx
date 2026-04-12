0cc561e5416cd8a59f690f2624392855373ff80b
tarun101727 <gaddamtarun157@gmail.com>
2026-04-12 13:55:48 +0530

    Update EmployeeProfile.jsx

--- src/PROFILE/EmployeeProfile.jsx
+++ src/PROFILE/EmployeeProfile.jsx
@@ -367,7 +367,7 @@ const EmployeeProfile = ({ user, notification, clear, readOnly }) => {
       )}
      {/* COMMENTS */}
 <div className="mt-12">
-  <ProfileComments profileId={user?._id} currentUserId={user?._id} />
+  <ProfileComments profileId={user?._id} />
 </div>
      
     </>

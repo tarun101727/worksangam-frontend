0db68a7dd52ac9ecc705facfda1b6449994f20e5
tarun101727 <gaddamtarun157@gmail.com>
2026-04-15 20:17:38 +0530

    Update socket.js

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -5,8 +5,5 @@ import { BASE_URL } from "../config"; // make sure config.js exports BASE_URL
 
 export const socket = io(BASE_URL, {
   withCredentials: true,
-  transports: ["websocket"], // 🔥 IMPORTANT FIX
-  auth: {
-    userId: Cookies.get("userId"),
-  },
+  auth: { userId: Cookies.get("userId") },
 });

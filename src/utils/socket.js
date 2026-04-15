93cb7f04f31fe5cc3c6c3b9e88f921d6c51c7668
tarun101727 <gaddamtarun157@gmail.com>
2026-04-15 23:49:59 +0530

    Update socket.js

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -1,9 +1,9 @@
+// src/utils/socket.js
 import { io } from "socket.io-client";
 import Cookies from "js-cookie";
-import { BASE_URL } from "../config";
+import { BASE_URL } from "../config"; // make sure config.js exports BASE_URL
 
 export const socket = io(BASE_URL, {
   withCredentials: true,
-  transports: ["websocket", "polling"], // force websocket first
   auth: { userId: Cookies.get("userId") },
 });

1f871ad1999b5f978a3d48a35a30f4ff6757271e
tarun101727 <gaddamtarun157@gmail.com>
2026-04-19 16:35:05 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -492,7 +492,7 @@ d="M8 10h.01M12 10h.01M16 10h.01M21 12c0 4.418-4.03
           {/* Fallback if no notifications */}
     {[...notifications, ...chatNotifications].length === 0 && (
       <div className="p-4 text-center text-white/60">
-        No notifications
+        {t("No notifications")}
       </div>
     )}
   </div>

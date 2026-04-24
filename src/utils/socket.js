fa122c4263faa0f3832f4fad1371a187f639feec
tarun101727 <gaddamtarun157@gmail.com>
2026-04-24 19:38:31 +0530

    Update socket.js

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -1,9 +1,7 @@
-// src/utils/socket.js
 import { io } from "socket.io-client";
-import Cookies from "js-cookie";
-import { BASE_URL } from "../config"; // make sure config.js exports BASE_URL
+import { BASE_URL } from "../config";
 
 export const socket = io(BASE_URL, {
+  autoConnect: false, // 🔥 IMPORTANT
   withCredentials: true,
-  auth: { userId: Cookies.get("userId") },
 });

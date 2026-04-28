666e99e862c2cd7ff4b3d87173faae7134f1d63f
tarun101727 <gaddamtarun157@gmail.com>
2026-04-28 22:54:03 +0530

    Update ChatPage.jsx

--- src/ChatPage.jsx
+++ src/ChatPage.jsx
@@ -323,7 +323,7 @@ if (loading && messages.length === 0) {
 
 return(
 
-<div className="flex flex-col h-screen">
+<div className="relative flex flex-col min-h-screen">
 
 {/* MESSAGE AREA */}
 <div
